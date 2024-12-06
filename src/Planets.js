93d5298803710199f321b33afa2d7c88b26f817b
BrunoFerMarques <brunofernandesmarques259@gmail.com>
2024-12-06 16:59:21 -0300

    Desbugando

--- src/Planets.js
+++ src/Planets.js
@@ -1,4 +1,5 @@
-import * as THREE from 'three';
+import * as THREE from "./node_modules/three/build/three.module.js";
+
 
 
 let planets = [];
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from "./node_modules/three/build/three.module.js";
+
+import { OrbitControls } from './node_modules/three/addons/controls/OrbitControls.js';
 import { loadAirplane, updateAirplane, updateCamera } from './Aircraft.js';
 import { loadPlanets, rotatePlanets} from './Planets.js';
 
