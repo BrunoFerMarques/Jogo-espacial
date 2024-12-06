c6ef786bf93689d4881a5e00e82a592f554049b7
BrunoFerMarques <brunofernandesmarques259@gmail.com>
2024-12-06 16:26:08 -0300

    Para desenvolvimento e testes

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+export default {
+    base: '/jogoespacial/',  // Ajuste para o nome do seu repositório
+    build: {
+      outDir: 'dist',  // Certifique-se de que a pasta de saída está correta
+    },
+}
